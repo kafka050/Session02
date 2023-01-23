bc352692dc0532a5f2d5dab5f48aa39e511185af
kafka050 <kafka050@umn.edu>
2023-01-23 13:12:12 -0600

    New Members are now added to member list

--- resources/js/new_member.js
+++ resources/js/new_member.js
@@ -1,10 +1,11 @@
+const memberList = document.getElementById("memberList")
 const input = document.getElementById("name input")
 const submit = document.getElementById("submit")
 
 submit.addEventListener("click", () => {
-    console.log("hi")
+    console.log("Adding member")
     const newMember = input.value
-    const member = document.createElement("p")
+    const member = document.createElement("li")
     member.innerText = newMember
-    document.querySelector("body").appendChild(member)
+    memberList.appendChild(member)
 })
